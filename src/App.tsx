b8756e86e2ecbfb4f5081c3de69e03ab2f8f7fba
Mridul Barman <mridulbarman027@gmail.com>
2022-05-24 20:24:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/navBar';
+import SideBar from './components/sideBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
