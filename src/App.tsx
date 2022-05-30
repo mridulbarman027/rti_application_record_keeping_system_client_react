e1a749143807a7cd4df62d87197667c3caa86619
Mridul Barman <mridulbarman027@gmail.com>
2022-05-30 12:07:51 +0530

    added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css';
-import NavBar from './components/navBar';
-import SideBar from './components/sideBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AdminIndex from './pages/admin';
+import AdminHome from './pages/admin/home';
+import AdminLogin from './pages/admin/login';
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <SideBar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> } /> 
+        <Route path='/login' element={ <Login /> } /> 
+        <Route path='/signup' element={ <Signup /> } /> 
+        <Route path='/admin' element={ <AdminIndex /> }>
+          <Route index element={ <AdminHome /> } />
+          <Route path='/adminLogin' element={ <AdminLogin /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
