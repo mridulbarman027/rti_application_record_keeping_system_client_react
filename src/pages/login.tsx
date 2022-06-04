07db2f3ba80c28a6905333648411b7699943165e
Mridul Barman <mridulbarman027@gmail.com>
2022-06-04 19:05:43 +0530

    created login & signup page ui

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import LoginSignupNavbar from '../components/Common/Navbar/LoginSignupNavbar';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='w-full h-full flex justify-center items-center flex-col'>
+
+      <LoginSignupNavbar />
+
+      <div className='h-full min-h-[calc(100vh-4rem)] flex justify-center items-center'>
+
+        <form>
+
+          <div className='flex flex-col mx-20 justify-center p-4 items-center border-[1px] border-slate-400 rounded-lg'>
+
+            <div className='font-semibold text-lg text-blue-600 mb-4'>Login your account</div>
+
+            <div className='w-full flex mb-4'>
+              <div className='w-24 text-left flex items-center'>Phone: </div>
+              <input type='text' maxLength={10} name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+            </div>
+
+            <div className='w-full flex mb-4'>
+              <div className='w-24 text-left flex items-center'>Password: </div>
+              <input type='text' name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+            </div>
+
+            <input className='border-[1px] mb-4 border-blue-800 bg-blue-600 hover:bg-blue-900 cursor-pointer text-white rounded-md pl-10 pr-10 pt-[4px] pb-[6px] flex items-center justify-center' type="submit" value='Login' />
+
+            <a href='/signup'><div className='font-semibold text-sm text-red-400 mb-4 hover:underline'>Create new account</div></a>
+
+          </div>
+
+        </form>
+
+      </div>
+
+
+
+    </div>
   )
 }
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import LoginSignupNavbar from '../components/Common/Navbar/LoginSignupNavbar';
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='w-full h-full flex justify-center items-center flex-col'>
+
+      <LoginSignupNavbar />
+
+      <div className='h-full min-h-[calc(100vh-4rem)] flex justify-center items-center'>
+
+        <form>
+
+          <div className='flex flex-col mx-20 justify-center p-4 items-center border-[1px] border-slate-400 rounded-lg'>
+
+            <div className='font-semibold text-lg text-blue-600 mb-4'>Create your account</div>
+
+            <div className='w-full flex mb-4'>
+              <div className='w-24 text-left flex items-center'>Name: </div>
+              <input type='text' name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+            </div>
+
+            <div className='w-full flex mb-4'>
+              <div className='w-24 text-left flex items-center'>Email: </div>
+              <input type='email' name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+            </div>
+            
+            <div className='w-full flex mb-4'>
+              <div className='w-24 text-left flex items-center'>Phone: </div>
+              <input type='text' maxLength={10} name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+            </div>
+
+            <div className='w-full flex mb-4'>
+              <div className='w-24 text-left flex items-center'>Password: </div>
+              <input type='text' name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+            </div>
+
+            <input className='border-[1px] mb-4 border-blue-800 bg-blue-600 hover:bg-blue-900 cursor-pointer text-white rounded-md pl-10 pr-10 pt-[4px] pb-[6px] flex items-center justify-center' type="submit" value='Signup' />
+
+            <a href='/login'><div className='font-semibold text-sm text-blue-400 mb-4 hover:underline'>Go to login page</div></a>
+
+          </div>
+
+        </form>
+
+      </div>
+
+
+
+    </div>
   )
 }
 
