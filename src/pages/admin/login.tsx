2c44d7b6378889b5cf5244f2b4d06a5b1b050509
Mridul Barman <mridulbarman027@gmail.com>
2022-06-24 14:23:05 +0530

    added mantine & admin login form submit

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { LoadingOverlay, Modal } from '@mantine/core';
+import React, { useState } from 'react'
 import AdminNavbar from '../../components/Common/Navbar/AdminNavbar';
 
 function AdminLogin() {
+
+  const [visible, setVisible] = useState(false);
+
+  const [modalOpened, setModalOpened] = useState(false);
+  const [modalTitle, setModalTitle] = useState("");
+
+  const submitLogin = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const target = event.target as typeof event.target & {
+      username: { value: string };
+      password: { value: string };
+    };
+    const username = target.username.value;
+    const password = target.password.value;
+
+    if (!username && username.length < 4) {
+      setModalTitle("Invalid username")
+      setModalOpened(true);
+      return;
+    }
+
+    if (!password && password.length < 6) {
+      setModalTitle("Invalid password")
+      setModalOpened(true);
+      return;
+    }
+
+    console.log(username + " " + password);
+    setVisible((v) => !v);
+  }
+
   return (
     <div className='w-full h-full flex justify-center items-center flex-col'>
 
@@ -9,7 +42,17 @@ function AdminLogin() {
 
       <div className='h-full min-h-[calc(100vh-4rem)] flex justify-center items-center'>
 
-        <form>
+        <LoadingOverlay visible={visible} />
+
+        <Modal
+          opened={modalOpened}
+          onClose={() => setModalOpened(false)}
+          title="Login alert"
+        >
+          <span>{modalTitle}</span>
+        </Modal>
+
+        <form onSubmit={submitLogin}>
 
           <div className='flex flex-col mx-20 justify-center p-4 items-center border-[1px] border-slate-400 rounded-lg'>
 
@@ -17,12 +60,12 @@ function AdminLogin() {
 
             <div className='w-full flex mb-4'>
               <div className='w-24 text-left flex items-center'>Username: </div>
-              <input type='text' maxLength={20} name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+              <input type='text' maxLength={20} name='username' className='w-60 p-1 border-[1px] text-base ml-2' />
             </div>
 
             <div className='w-full flex mb-4'>
               <div className='w-24 text-left flex items-center'>Password: </div>
-              <input type='text' name='asdas' className='w-60 p-1 border-[1px] text-base ml-2' />
+              <input type='password' name='password' className='w-60 p-1 border-[1px] text-base ml-2' />
             </div>
 
             <input className='border-[1px] mb-4 border-blue-800 bg-blue-600 hover:bg-blue-900 cursor-pointer text-white rounded-md pl-10 pr-10 pt-[4px] pb-[6px] flex items-center justify-center' type="submit" value='Login' />
