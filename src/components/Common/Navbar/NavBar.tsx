1cca44b834e7dbe4dfff75bafcb3debdd85d4543
Mridul Barman <mridulbarman027@gmail.com>
2022-07-05 21:31:28 +0530

    refactored

--- src/components/Common/Navbar/NavBar.tsx
+++ src/components/Common/Navbar/NavBar.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     return (
         <div className='bg-[#ccc] h-28 w-full flex items-center'>
 
-            <div className='font-bold text-3xl mx-10 '>RTI APPLICATION RECORD KEEPING SYSTEM</div>
+            <Link to={'/'}>
+                <div className='font-bold text-3xl mx-10 '>RTI APPLICATION RECORD KEEPING SYSTEM</div>
+            </Link>
 
         </div>
     )
